1ada0f90e2513a6f4bc6ad1a561de0a35bcdce11
D L <lazykindit@yandex.ru>
2023-10-17 12:54:19 +0300

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,31 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T> {
+    private List<T> data;
+    private int index;
+
+    public CyclicIterator(List<T> data) {
+        this.data = new ArrayList<>(data);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index >= data.size()) {
+            index = 0;
+        }
+        return !data.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data.get(index++);
+    }
+}
